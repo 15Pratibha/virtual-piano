74e66a84ba646426bbc4d97591a1c54eb055dcbf
Pratibha patel <pratibhapt18@gmail.com>
2017-07-14 02:01:18 +0530

    change js file

--- main2..js
+++ main2..js
@@ -1,166 +1,316 @@
+
+////////////
+////////////
+///////
 Synth instanceof AudioSynth; // true
 
 var testInstance = new AudioSynth;
 testInstance instanceof AudioSynth; // true
 
-testInstance === Synth; 
-windows.onload=function(){
+testInstance ==Synth;
+window.onload=function(){
 	var instrument=0;}
 	//for piano
 	$('#piano').on('click',function(){
-		innstrument=0;
+		instrument=0;
 	})
 	$('#organ').on('click',function(){
-		innstrument=1;
+		instrument=1;
 	})
 	$('#guitar').on('click',function(){
-		innstrument=2;
+		instrument=2;
 	})
 	$('#edm').on('click',function(){
-		innstrument=3;
+		instrument=3;
 	})
-$('key_C,1').on('click',function(){
+
+$('#key_C1').on('click',function(){
+
 	Synth.play(0,'C',3,2);
+
 	console.log(Synth.play(instrument,'C',3,2));
+
 });
-$('key_C,#').on('click',function(){
-	Synth.play(0,'#3',3,2);
+
+$('#key_C#').on('click',function(){
+
+	Synth.play(0,'C#',3,2);
+
 	console.log(Synth.play(instrument,'C#',3,2));
+
 });
-$('key_D,1').on('click',function(){
+
+$('#key_D,1').on('click',function(){
+
 	Synth.play(0,'D',3,2);
+
 	console.log(Synth.play(instrument,'D',3,2));
+
 });
-$('key_D,1').on('click',function(){
+
+$('#key_D1').on('click',function(){
+
 	Synth.play(0,'D',3,2);
+
 	console.log(Synth.play(innstrument,'D',3,2);
+
 });
 
-$('KEY_E,1').on('click',function(){
+
+
+$('#KEY_E1').on('click',function(){
+
 	Synth.play(0,'E',3,2);
+
 	console.log(Synth.play(0,'E',3,2));
-});
-$('KEY_F,-1').on('click',function(){
-	Synth.play(0,'F',3,2);
-	console.log(Synth.play(0,'F',3,2));
-});
-$('KEY_F#,-1').on('click',function(){
-	Synth.play(0,'F#',3,2);
-	console.log(Synth.play(0,'F#',3,2));
-});
-$('KEY_G,-1').on('click',function(){
-	Synth.play(0,'G',3,2);
-	console.log(Synth.play(0,'G',3,2));
-});
-$('KEY_F#,-1').on('click',function(){
-	Synth.play(0,'F#',3,2);
-	console.log(Synth.play(0,'F#',3,2));
-});
-$('KEY_A,-1').on('click',function(){
-	Synth.play(0,'A',3,2);
-	console.log(Synth.play(0,'A',3,2));
-});
-$('KEY_A#,-1').on('click',function(){
-	Synth.play(0,'A#',3,2);
-	console.log(Synth.play(0,'A#',3,2));
-});
-$('KEY_B,-1').on('click',function(){
-	Synth.play(0,'B',3,2);
-	console.log(Synth.play(0,'B',3,2));
-});
-$('KEY_C,-1').on('click',function(){
-	Synth.play(0,'C',4,2);
-	console.log(Synth.play(0,'C',4,2));
-});
-$('KEY_C#,0').on('click',function(){
-	Synth.play(0,'C#',4,2);
-	console.log(Synth.play(0,'C#',4,2));
-});
-$('KEY_D,0').on('click',function(){
-	Synth.play(0,'D',4,2);
-	console.log(Synth.play(0,'D',4,2));
-});
-$('KEY_D#,-1').on('click',function(){
-	Synth.play(0,'D#',4,2);
-	console.log(Synth.play(0,'D#',4,2));
-});
-$('KEY_E,0').on('click',function(){
-	Synth.play(0,'E',4,2);
-	console.log(Synth.play(0,'E',4,2));
-});
-$('KEY_F,0').on('click',function(){
-	Synth.play(0,'F',4,2);
-	console.log(Synth.play(0,'F',4,2));
-});
-$('KEY_D#,0').on('click',function(){
-	Synth.play(0,'D#',4,2);
-	console.log(Synth.play(0,'D#',4,2));
-});
-$('KEY_G,0').on('click',function(){
-	Synth.play(0,'G',4,2);
-	console.log(Synth.play(0,'G',4,2));
+
 });
 
-$('KEY_G#,0').on('click',function(){
+$('#KEY_F-1').on('click',function(){
+
+	Synth.play(0,'F',3,2);
+
+	console.log(Synth.play(0,'F',3,2));
+
+});
+
+$('#KEY_F#1').on('click',function(){
+
+	Synth.play(0,'F#',3,2);
+
+	console.log(Synth.play(0,'F#',3,2));
+
+});
+
+$('#KEY_G-1').on('click',function(){
+
+	Synth.play(0,'G',3,2);
+
+	console.log(Synth.play(0,'G',3,2));
+
+});
+
+$('#KEY_F#1').on('click',function(){
+
+	Synth.play(0,'F#',3,2);
+
+	console.log(Synth.play(0,'F#',3,2));
+
+});
+
+$('#KEY_A-1').on('click',function(){
+
+	Synth.play(0,'A',3,2);
+
+	console.log(Synth.play(0,'A',3,2));
+
+});
+
+$('#KEY_A#1').on('click',function(){
+
+	Synth.play(0,'A#',3,2);
+
+	console.log(Synth.play(0,'A#',3,2));
+
+});
+
+$('#KEY_B-1').on('click',function(){
+
+	Synth.play(0,'B',3,2);
+
+	console.log(Synth.play(0,'B',3,2));
+
+});
+
+$('#KEY_C-1').on('click',function(){
+
+	Synth.play(0,'C',4,2);
+
+	console.log(Synth.play(0,'C',4,2));
+
+});
+
+$('#KEY_C#0').on('click',function(){
+
+	Synth.play(0,'C#',4,2);
+
+	console.log(Synth.play(0,'C#',4,2));
+
+});
+
+$('#KEY_D0').on('click',function(){
+
+	Synth.play(0,'D',4,2);
+
+	console.log(Synth.play(0,'D',4,2));
+
+});
+
+$('#KEY_D#1').on('click',function(){
+
+	Synth.play(0,'D#',4,2);
+
+	console.log(Synth.play(0,'D#',4,2));
+
+});
+
+$('#KEY_E0').on('click',function(){
+
+	Synth.play(0,'E',4,2);
+
+	console.log(Synth.play(0,'E',4,2));
+
+});
+
+$('#KEY_F0').on('click',function(){
+
+	Synth.play(0,'F',4,2);
+
+	console.log(Synth.play(0,'F',4,2));
+
+});
+
+$('#KEY_D#0').on('click',function(){
+
+	Synth.play(0,'D#',4,2);
+
+	console.log(Synth.play(0,'D#',4,2));
+
+});
+
+$('#KEY_G0').on('click',function(){
+
+	Synth.play(0,'G',4,2);
+
+	console.log(Synth.play(0,'G',4,2));
+
+});
+
+
+
+$('#KEY_G#0').on('click',function(){
+
 	Synth.play(0,'G#',4,2);
+
 	console.log(Synth.play(0,'G#',4,2));
+
 });
-$('KEY_A,0').on('click',function(){
+
+$('#KEY_A0').on('click',function(){
+
 	Synth.play(0,'A',4,2);
+
 	console.log(Synth.play(0,'A',4,2));
+
 });
-$('KEY_A#,0').on('click',function(){
+
+$('#KEY_A#0').on('click',function(){
+
 	Synth.play(0,'A#',4,2);
+
 	console.log(Synth.play(0,'A#',4,2));
+
 });
-$('KEY_B,0').on('click',function(){
+
+$('#KEY_B0').on('click',function(){
+
 	Synth.play(0,'B',4,2);
+
 	console.log(Synth.play(0,'B',4,2));
+
 });
-$('KEY_C,1').on('click',function(){
+
+$('#KEY_C1').on('click',function(){
+
 	Synth.play(0,'C',5,2);
+
 	console.log(Synth.play(0,'C',5,2));
+
 });
-$('KEY_C#,1').on('click',function(){
+
+$('#KEY_C#1').on('click',function(){
+
 	Synth.play(0,'C#',5,2);
+
 	console.log(Synth.play(0,'C#',5,2));
+
 });
-$('KEY_D,1').on('click',function(){
+
+$('#KEY_D1').on('click',function(){
+
 	Synth.play(0,'D',5,2);
+
 	console.log(Synth.play(0,'D',5,2));
-});$('KEY_D#,1').on('click',function(){
+
+});
+$('#KEY_D#1').on('click',function(){
+
 	Synth.play(0,'D#',5,2);
+
 	console.log(Synth.play(0,'D#',5,2));
+
 });
-$('KEY_E,1').on('click',function(){
+
+$('#KEY_E1').on('click',function(){
+
 	Synth.play(0,'E',5,2);
+
 	console.log(Synth.play(0,'E',5,2));
+
 });
-$('KEY_F,1').on('click',function(){
+
+$('#KEY_F1').on('click',function(){
+
 	Synth.play(0,'F',5,2);
+
 	console.log(Synth.play(0,'F',5,2));
+
 });
-$('KEY_F#,1').on('click',function(){
+
+$('#KEY_F#1').on('click',function(){
+
 	Synth.play(0,'F#',5,2);
+
 	console.log(Synth.play(0,'F#',5,2));
+
 });
-$('KEY_G,1').on('click',function(){
+
+$('#KEY_G1').on('click',function(){
+
 	Synth.play(0,'G',5,2);
+
 	console.log(Synth.play(0,'G',5,2));
+
 });
-$('KEY_G#,1').on('click',function(){
+
+$('#KEY_G#1').on('click',function(){
+
 	Synth.play(0,'G#',5,2);
+
 	console.log(Synth.play(0,'G#',5,2));
+
 });
-$('KEY_A,1').on('click',function(){
+
+$('#KEY_A1').on('click',function(){
+
 	Synth.play(0,'A',5,2);
+
 	console.log(Synth.play(0,'A',5,2));
+
 });
-$('KEY_A#,1').on('click',function(){
+
+$('#KEY_A#1').on('click',function(){
+
 	Synth.play(0,'A#',5,2);
+
 	console.log(Synth.play(0,'A#',5,2));
+
 });
-$('KEY_B,1').on('click',function(){
+
+$('#KEY_B1').on('click',function(){
+
 	Synth.play(0,'B',5,2);
+
 	console.log(Synth.play(0,'B',5,2));
+
 });
--- piano.js
+++
@@ -1,4 +0,0 @@
-var Synth=new audiosynth()
-$('#key_C').on('click',function(){
-  Synth.play(0,'C',3,2)
-});
